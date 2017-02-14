19b89631f8b906d43594746d5f2455569bf22ed4
Christian Wunder <wunderman4@gmail.com>
2017-02-14 16:36:46 -0700

    first Commit

---
+++ src/ManyToMany/API/ActorsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ManyToMany.Data;
+using ManyToMany.Models;
+using ManyToMany.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ManyToMany.API
+{
+    [Route("api/[controller]")]
+    public class ActorsController : Controller
+    {
+        ApplicationDbContext _db;
+
+        public ActorsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public List<Actor> Get()
+        {
+            return _db.Actors.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActorWithMovies Get(int id)
+        {
+            ActorWithMovies actor = (from a in _db.Actors
+                                     where a.Id == id
+                                     select new ActorWithMovies
+                                     {
+                                         Id = a.Id,
+                                         FirstName = a.FirstName,
+                                         LastName = a.LastName,
+                                         Movies = (from am in _db.MovieActors
+                                                   where am.ActorId == id
+                                                   select am.Movie).ToList()
+                                     }).FirstOrDefault();
+            return actor;
+        }
+       
+    }
+}
---
+++ src/ManyToMany/API/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ManyToMany.Data;
+using ManyToMany.ViewModels;
+using ManyToMany.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ManyToMany.API
+{
+    [Route("api/[controller]")]
+    public class MoviesController : Controller
+    {
+        // this is only here bacuse we are not using repo or services 
+        private ApplicationDbContext _db;
+        //Constructor
+        public MoviesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public List<Movie> Get()
+        {
+            return _db.Movies.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public MovieWithActors Get(int id)
+        {
+            MovieWithActors Mov = (from m in _db.Movies
+                                   where m.Id == id
+                                   select new MovieWithActors
+                                   {
+                                       Id = m.Id,
+                                       Title = m.Title,
+                                       Director = m.Director,
+                                       Actors = (from ma in _db.MovieActors // this is the join table
+                                                 where ma.MovieId == m.Id // where the id's match
+                                                 select ma.Actor).ToList() // select the actor and add to list
+                                   }).FirstOrDefault(); // return first or default like normal.
+            return Mov;
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post(int id,[FromBody]Movie mov)
+        {
+            if (mov == null)
+            {
+                return BadRequest();
+            } else if (mov.Id == 0)
+            {
+                Movie tempMov = new Movie
+                {
+                    Title = mov.Title,
+                    Director = mov.Director,
+                };
+                _db.Movies.Add(tempMov);
+                _db.SaveChanges();
+                _db.MovieActors.Add(new MovieActor
+                {
+                    MovieId = id,
+                    ActorId = // ahhhh needs to pass in the actor id. 
+                })
+                
+
+                //foreach (Actor actor in mov.) // --------------- to select 
+                //{ 
+                //     _db.MovieActors.Add(new MovieActor
+                //     {
+                //         MovieId = movLookUp.Id, //-------------------this needs to be the newly assigned move id.
+                //         ActorId = actor.Id
+                //     });
+                //    _db.SaveChanges();
+                //}
+
+
+                return Ok();
+
+            } else
+            {
+                return BadRequest();
+            }
+        }
+
+
+    }
+}
